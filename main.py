ffb0fe3e1fa577dfcf016d2093168f5ab3ec1fbf
Alexsander <alelin36@edu.umea.se>
2026-09-17 13:40:54 +0200

    början på beretelsen

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Här skriver du ditt textäventyr
+
+player1 = input("Player1-Name")
+print(player1+"-vaknar upp från sin säng klär på sig och går ut och ser sin kolega")
+print ("vill du säga hej till din kolega...") 
+Hey = input ("ja eller nej")
+if Hey == ("ja"):
+    print("hej-"+player1+"-vad trevligt att se dig idag")
+    print("trevligt att se dig idag")
+else:
+    print(player1+"-ignorerade din kolega")
+    print("det kommer hon att komma ihåg")
+print("du fortsätter och att gå tills du ser en korsning vill du gå vänster eller höger")
+Vänsterellerhöger = input("vänster eller höger")
+if Vänsterellerhöger == ("vänster"):
+    print(player1+("-toge en vänster i korsningen och komm fram till "))
